6f3e24745027bdbb6fae915ac2fb2cdf3423c1ff
Habuon <danci99@protonmail.com>
2020-09-29 08:39:55 +0200

    Fixed some additional errors

--- hanoi.c
+++ hanoi.c
@@ -8,7 +8,7 @@ void hanoi(int n, int a, int b)
   }
   else
   {
-    hanoi(n - 1, a, 6 - a);
+    hanoi(n - 1, a, 6 - a - b);
     printf("presunul som kamen %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
